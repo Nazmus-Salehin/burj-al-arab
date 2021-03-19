2f6b8b0c0d3eb406e3d0c63fba74c64625c3a4a1
Nazmus-Salehin <16101086@uap-bd.edu>
2021-03-19 20:50:01 +0600

    first commit

---
+++ src/components/Login/firebase.config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCEXFQnCe_XJDQUpvr_FYuo_Motrh-OqtA",
+    authDomain: "burj-al-arab-36f5d.firebaseapp.com",
+    projectId: "burj-al-arab-36f5d",
+    storageBucket: "burj-al-arab-36f5d.appspot.com",
+    messagingSenderId: "64118445478",
+    appId: "1:64118445478:web:0c7c61b6f12cf0a29c2b67"
+  };
+
+  export default firebaseConfig;
